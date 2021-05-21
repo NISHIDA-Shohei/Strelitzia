b542606b190d7526fd1a6958514fd36cc09bb37f
NISHIDA Shohei <shohei22cosmos@gmail.com>
2021-05-21 09:24:48 +0900

    Initial Commit

---
+++ Strelitzia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Strelitzia
+//
+//  Created by 西田翔平 on 2021/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
