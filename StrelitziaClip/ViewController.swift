718ec4328f3c399a8115896b71d79abc27a9883d
NISHIDA Shohei <shohei22cosmos@gmail.com>
2021-06-07 13:56:49 +0900

    clipsのtargetの追加

---
+++ StrelitziaClip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StrelitziaClip
+//
+//  Created by 西田翔平 on 2021/06/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
